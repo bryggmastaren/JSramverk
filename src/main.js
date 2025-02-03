190a087efaf29191badaa112f2587b47c2ea93f1
bryggmastaren <charlotte.larsson01@gmail.com>
2025-02-03 14:37:21 +0100

    media commit osv

--- src/main.js
+++ src/main.js
@@ -1,5 +1,24 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+createApp(App).mount("#app");
+
+Vue.createApp({
+  data() {
+    return {
+      data: [], // Här kommer API-datan att lagras
+    };
+  },
+  mounted() {
+    // Hämta data när appen är monterad
+    fetch(
+      "https://api.nasa.gov/planetary/apod?api_key=bLKsuQaFpHG3mK8eo9UwyCI1RXpF9CKf8DqVYmiW&start_date=2025-01-30"
+    ) // exempel-API
+      .then((response) => response.json())
+      .then((data) => {
+        this.data = data; // Sätt värdet till vår data
+      })
+      .catch((error) => console.error("Fetch error:", error));
+  },
+}).mount("#app");
